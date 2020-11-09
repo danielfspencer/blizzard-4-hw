af8937305c354a9b2704ae2830cc2346aaeba0ef
Daniel Spencer <danielfspencer1@gmail.com>
2020-11-09 21:37:17 +0000

    interface: add set_mem function

--- models/model.js
+++ models/model.js
@@ -1,9 +1,18 @@
 class Model {
     destroy () {}
 
-    set_rom(input) {
-        // loop through each line and do
-        // this.write(value, dest)
+    set_mem(input, dest) {
+        let array = input.trim().split("\n")
+
+        for (let i = 0; i < array.length; i++) {
+            let number = parseInt(array[i], 2)
+
+            if (number >= 0 && number <= 0xffff) {
+                this.write(number, dest+ i)
+            } else {
+                throw new Error(`Illegal memory input, word ${i}: '${array[i]}'`)
+            }
+        }
     }
 }
 

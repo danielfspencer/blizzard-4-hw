6f0a4b3b7710c26768c6da153c477b39439bd1f7
Daniel Spencer <danielfspencer1@gmail.com>
2020-11-24 10:58:49 +0000

    test: unit: rom: merge r/w copy test into one file

--- tests/unit/rom/rom.js
+++ tests/unit/rom/rom.js
@@ -14,3 +14,19 @@ describe('copy', () => {
     }
   })
 })
+
+describe('read/write', () => {
+  test.each(MODELS)('%s', model => {
+    for (let addr = 0x8000; addr <= 0xffff; addr++) {
+      let value = random_word()
+
+      model.write(value, addr)
+
+      let result = model.read(addr)
+
+      if (result !== value) {
+        throw new Error(`Read ${addr} did not equal ${value} (got ${result})`)
+      }
+    }
+  })
+})

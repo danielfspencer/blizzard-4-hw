7ca389d9d3d6bc5c46b85bf9b0ea24020918f896
Daniel Spencer <danielfspencer1@gmail.com>
2020-11-28 18:27:16 +0000

    test: integr: text: fix indentation

--- tests/integration/consistency/text_editor.js
+++ tests/integration/consistency/text_editor.js
@@ -13,51 +13,51 @@ describe('text editor', () => {
 
     let ps2_bytes = []
     for (let string of strings) {
-        for (let char of string) {
-            let code
-            let is_uppercase = false
-            if (char === " ") {
-                code = "Space"
-            } else {
-                is_uppercase = char === char.toUpperCase()
-                char = char.toUpperCase()
-                code = `Key${char}`
-            }
-
-            if (is_uppercase) {
-                // shift down (for uppercase)
-                ps2_bytes.push(...keypress_to_scancode("ShiftLeft", true))
-            }
-            // keydown
-            ps2_bytes.push(...keypress_to_scancode(code, true))
-
-            // keyup
-            ps2_bytes.push(...keypress_to_scancode(code, false))
-
-            if (is_uppercase) {
-                // shift up (for uppercase)
-                ps2_bytes.push(...keypress_to_scancode("ShiftLeft", false))
-            }
+      for (let char of string) {
+        let code
+        let is_uppercase = false
+        if (char === " ") {
+          code = "Space"
+        } else {
+          is_uppercase = char === char.toUpperCase()
+          char = char.toUpperCase()
+          code = `Key${char}`
         }
 
-        // return (newline)
-        ps2_bytes.push(...keypress_to_scancode("Enter", true))
-        ps2_bytes.push(...keypress_to_scancode("Enter", false))
+        if (is_uppercase) {
+          // shift down (for uppercase)
+          ps2_bytes.push(...keypress_to_scancode("ShiftLeft", true))
+        }
+        // keydown
+        ps2_bytes.push(...keypress_to_scancode(code, true))
+
+        // keyup
+        ps2_bytes.push(...keypress_to_scancode(code, false))
+
+        if (is_uppercase) {
+          // shift up (for uppercase)
+          ps2_bytes.push(...keypress_to_scancode("ShiftLeft", false))
+        }
+      }
+
+      // return (newline)
+      ps2_bytes.push(...keypress_to_scancode("Enter", true))
+      ps2_bytes.push(...keypress_to_scancode("Enter", false))
     }
 
 
     // spread adding the bytes to the buffer over the length of the simulation
     // this stresses the fifo more because it is being written to and read from at the same time
     for (let i = 0; i < 10; i++) {
-        if (ps2_bytes.length > 0) {
-            for (let bytes = 0; bytes < 30; bytes++) {
-                let byte = ps2_bytes.shift()
-                if (byte !== undefined) {
-                    model.send_ps2_bytes([byte])
-                }
-            }
+      if (ps2_bytes.length > 0) {
+        for (let bytes = 0; bytes < 30; bytes++) {
+          let byte = ps2_bytes.shift()
+          if (byte !== undefined) {
+            model.send_ps2_bytes([byte])
+          }
         }
-        model.step(CYCLES * (1/10))
+      }
+      model.step(CYCLES * (1/10))
     }
   })
 })
